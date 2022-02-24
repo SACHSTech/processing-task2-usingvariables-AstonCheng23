df5c5e972c46320883038a1df6ab09e34f32d4a9
Aston Cheng <78488373+AstonCheng23@users.noreply.github.com>
2022-02-24 17:09:53 +0000

    .

--- Sketch.java
+++ Sketch.java
@@ -1,11 +1,17 @@
-import processing.core.PApplet;
+/*
+Name: Aston
+Date: 2/24/2022
+Project: Processing Task
+*/
 
+
+import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-  int xSize = 600;
-  int ySize = 600;
-
+  int xSize = 300;
+  int ySize = 300;
+  
   public void settings() {
     size(xSize, ySize);
   }
@@ -13,20 +19,23 @@ public class Sketch extends PApplet {
   //stuff is relative to (400, 400)
   float xSizeRatio = (float)xSize/(float)400.00;
   float ySizeRatio = (float)ySize/(float)400.00;
-
-
+  
   public void setup() {
     background(210, 255, 173);
   }
 
   public void draw() {
-
-    //System.out.println(xSize + " " + ySize + " " + xSizeRatio + " " + ySizeRatio
+    //System.out.println(xSize + " " + ySize + " " + xSizeRatio + " " + ySizeRatio);
+    
     stroke(128);
     line(150*xSizeRatio, 25*ySizeRatio, 270*xSizeRatio, 350*ySizeRatio);  
 
     stroke(255);
     line(50*xSizeRatio, 125*ySizeRatio, 70*xSizeRatio, 50*ySizeRatio);  
+
+    stroke(0);
+    fill(0);
+    ellipse(200*xSizeRatio, 200*ySizeRatio, 200*xSizeRatio, 200*ySizeRatio);
   }
   
 }
