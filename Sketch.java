bf4a2b43bc0eeae5d82cc85d47d3f3891732befe
Aston Cheng <78488373+AstonCheng23@users.noreply.github.com>
2022-02-25 01:11:03 +0000

    made easier to read (is it too much whitespace?)

--- Sketch.java
+++ Sketch.java
@@ -9,16 +9,16 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-  int xSize = 300;
-  int ySize = 300;
+  int xSize = 600;
+  int ySize = 600;
   
   public void settings() {
     size(xSize, ySize);
   }
 
   //stuff is relative to (400, 400)
-  float xSizeRatio = (float)xSize/(float)400.00;
-  float ySizeRatio = (float)ySize/(float)400.00;
+  float xSizeRatio = (float) xSize / (float) 400.00;
+  float ySizeRatio = (float) ySize / (float) 400.00;
   
   public void setup() {
     background(210, 255, 173);
@@ -28,14 +28,14 @@ public class Sketch extends PApplet {
     //System.out.println(xSize + " " + ySize + " " + xSizeRatio + " " + ySizeRatio);
     
     stroke(128);
-    line(150*xSizeRatio, 25*ySizeRatio, 270*xSizeRatio, 350*ySizeRatio);  
+    line(150 * xSizeRatio, 25 * ySizeRatio, 270 * xSizeRatio, 350 * ySizeRatio);  
 
     stroke(255);
-    line(50*xSizeRatio, 125*ySizeRatio, 70*xSizeRatio, 50*ySizeRatio);  
+    line(50 * xSizeRatio, 125 * ySizeRatio, 70 * xSizeRatio, 50 * ySizeRatio);  
 
     stroke(0);
     fill(0);
-    ellipse(200*xSizeRatio, 200*ySizeRatio, 200*xSizeRatio, 200*ySizeRatio);
+    ellipse(200 * xSizeRatio, 200 * ySizeRatio, 200 * xSizeRatio, 200 * ySizeRatio);
   }
   
 }
